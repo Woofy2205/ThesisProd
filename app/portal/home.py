829cf0339af6681f1d6113d7943a7b58b46e58ce
Nguyễn Duy Chiến <100187821+Woofy2205@users.noreply.github.com>
2025-03-02 02:49:09 +0700

    Update home.py title and instructions; implement PDF upload and processing in practice.py

--- app/portal/home.py
+++ app/portal/home.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-st.title("Chat with your Lecture Series 😁")
+st.title("Chat with your Lecture 😁")
 st.write(
-   """This is a chatbot that can answer questions about your lecture series. 
-   While displaying the section of videos or notes that are relevant to the question."""
+   """This is a chatbot that can create the questions from your lecture"""
 )
 
 col1, col2 = st.columns(2)
@@ -12,52 +11,14 @@ with col1:
    st.subheader("Instructions for Teacher")
    st.markdown(
       """
-1. **Logging In**
-   - Navigate to the **Teacher's Portal**.
-   - Enter the admin password in the sidebar to access admin features.
-   - If you’re not an admin, you can still view content in **Viewer Mode**.
-
-2. **Creating and Managing Courses**
-   - **Create a New Course**:
-   - Enter the desired course name in the input field and click "Create Course."
-   - **Add Lectures**:
-   - Click "➕" in the course tab to add a new lecture to the selected course.
-
-3. **Uploading Lecture Materials**
-   - Select a lecture and upload files (PDFs or MP4s) to associate them with the lecture.
-   - Uploaded files are listed under the corresponding lecture.
-
-4. **Processing Files**
-   - Click "Process Lectures" to prepare the uploaded files for student access.
-   - Processed files are marked with a ✅; unprocessed files show ⏳.
-
-5. **Previewing Files**
-   - Select a file from the lecture list to preview it in the **File Preview** section.
-
-6. **Managing Courses**
-   - Delete a course by clicking "Delete Course" in the corresponding tab.
-   - All associated lectures and files will also be removed.
-               """
+      Write Something here
+      """
    )
 
 with col2:
    st.subheader("Instructions for Student")
    st.markdown(
       """
-1. **Accessing the Student's Portal**
-   - Open the app and navigate to the **Student's Portal** section.
-   - Use the sidebar to select a course from the available list.
-
-2. **Features**
-   - **Lecture Q&A Chat**:
-   - Use the chat interface to ask questions about the selected course's lecture series.
-   - You can also filter the lectures you want to talk about from the menu above chat input. 
-   - **Artifacts (Documents and Videos)**:
-   - Relevant PDFs and videos are displayed based on your query.
-   - Click on "🔍" to open a full PDF viewer for detailed reading.
-   - Watch video clips directly in the app, with timestamps and transcripts provided.
-
-3. **Additional Options**
-   - **Clear Chat**: Reset the chat by clicking the "Clear Chat" button in the sidebar.
-               """
+      Write Something here
+      """
    )
--- app/portal/practice.py
+++ app/portal/practice.py
@@ -1,3 +1,130 @@
-import streamlit as st
+import os
 
-st.write('Hi, you\'re here to practice huh?')
+import streamlit as st
+from dotenv import load_dotenv
+from streamlit import session_state as ss
+from streamlit_pdf_viewer import pdf_viewer
+
+os.chdir("D:/Project/ThesisProd/")
+
+from core.ingestion.preprocessing.storage.FaissStore import FaissStore
+from core.llm.AssistantLLM import AssistantBot
+from core.llm.TeacherLLM import TeacherBot
+from core.retriever.Retriever import Retriever
+
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+
+def process_pdf(doc_path: str) -> list:
+    faiss_store = FaissStore(documents_path = doc_path)
+    nodes = faiss_store.get_nodes()
+    return nodes
+
+def create_store(nodes: list):
+    _store = Retriever(nodes = nodes)
+    return _store
+
+st.header("DocumentsQA Practice Mode :books:")
+
+if 'pdf' not in ss:
+    ss['pdf'] = None
+
+if 'store' not in ss:
+    ss['store'] = None
+
+if 'nodes' not in ss:
+    ss['nodes'] = None
+
+col1, col2 = st.columns([2,2])
+
+with col2:
+    subcol1, subcol2, subcol3 = st.columns([8,1,1])
+    pdf_file = subcol1.file_uploader("Upload PDF file", type=('pdf'), label_visibility = "collapsed")
+    
+    ss.pdf = pdf_file
+    save_folder = "app/static/pdfdir/"
+    
+    if subcol2.button("🚩", use_container_width=True):
+        with st.spinner("Teacher digesting the PDF"):
+            ss.nodes = process_pdf(save_folder)
+
+    if subcol3.button("👍", use_container_width=True):
+        with st.spinner("Teacher preparing questions..."):
+            ss.store = create_store(ss.nodes)
+    
+    # subcol1, subcol2, subcol3 = st.columns([8,1,1])
+    # if subcol1.button("Process PDF", use_container_width=True):
+    #     with st.spinner("Teacher digesting the PDF"):
+    #         ss.nodes = process_pdf(save_folder)
+
+    # if subcol2.button("Preparing questions", use_container_width=True):
+    #     with st.spinner("Teacher preparing questions..."):
+    #         ss.store = create_store(ss.nodes)
+    
+    if ss.pdf:
+        binary_data = ss.pdf.getvalue()
+        pdf_viewer(input=binary_data, width=1000, height=1000)
+    
+    if pdf_file is not None:
+        save_path = os.path.join(save_folder, pdf_file.name)
+        for file in os.listdir(save_folder):
+            os.remove(os.path.join(save_folder, file))
+        with open(save_path, mode='wb') as w:
+            w.write(pdf_file.getvalue())
+    else:
+        st.info("Please upload a PDF file.")
+            
+    if ss.store:
+        retriever = ss.store.get_retriever(top_k = 5)        
+        st.success("PDF processed successfully!")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+with col1:
+    mes_container = st.container(height=1150)
+    if prompt := st.chat_input("Type some topic you want to practice!"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        
+        context = ss.store.get_big_context(retriever=retriever, query=prompt)
+        
+        with st.spinner("Teacher is creating questions..."):
+            teacher = TeacherBot()
+            _response = teacher.create_question(context = context)
+            
+        for i in range(len(_response)):
+            for j in range(len(_response[i])):
+                # if j == 0:
+                #     _response[i][0] = "###" + _response[i][0]
+                if j == 5:
+                    _response[i][5] = ":green[" + _response[i][5] + "]"
+                if j == 6:
+                    _response[i][6] = ":green[" + _response[i][6] + "]"
+
+        # for i in range(len(respond)):
+        #     for j in range(len(respond[i])):
+        #         print(respond[i][j] + "\n")
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            for i in range(len(_response)):
+                for j in range(len(_response[i])):
+                    if j == 0:
+                        st.subheader(_response[i][j])
+                    else:
+                        st.markdown(_response[i][j])
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": _response})
+    
+
+os.chdir("D:/Project/ThesisProd/app/")
